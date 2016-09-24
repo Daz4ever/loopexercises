8aff44ad25677bd38c800694ca8b8486065d1e76
Dominic Zenon <dominiczenon@gmail.com>
2016-09-23 21:57:08 -0400

    initial

---
+++ loopexercises.py
@@ -0,0 +1,43 @@
+#prints numbers 1-10
+for number in range(1, 11):
+    print number
+
+#asks user for a range of numbers to prints such as 2-8 and prints them
+start = int(raw_input("Start from: "))
+end = int(raw_input("End on: "))
+
+for number in range(start, (end + 1)):
+    print number
+
+#prints odd number 1-9
+for number in range(1, 11, 2)
+    print number
+
+#prints a 5x5 board of *
+for i in range(0, 5):
+    print "*" * 5
+
+#prints a board of * based on the users requested size
+N = int(raw_input("How big is the square? "))
+
+for i in range(0, N):
+    print "*" * N
+
+#prints a board of * thats empty based on users height and width
+width = int(raw_input("Width? "))
+height = int(raw_input("Height? "))
+
+for i in range(0, height):
+    if i == 0 or i == height - 1:
+        print "*" * width
+    else:
+        print "*" + (" " * (width - 2)) + "*"
+
+# makes a triangle of *'s'
+spaces = 3
+stars = 1
+
+for i in range(0, 5):
+    print (" " * spaces) + ("*" * stars) + (" " * spaces)
+     stars += 2
+     space -= 1
